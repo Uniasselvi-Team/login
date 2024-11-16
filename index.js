f1908feb11baa80121c094b0d47e038373ee084a
Gabs Mart <GabrielMartins014@hotmail.com>
2024-11-16 10:39:48 -0300

    Organização Inicial do Projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+app.use(express.json());
+
+const conn = require('./db/conn');
+const User = require('./models/User');
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const {DataTypes} = require('sequelize');
+
+const db = require('../db/conn');
+
+const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user'
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+
+module.exports = User;
